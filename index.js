1618d2f7420730d5d56439ebd132cd096fa0a30d
mahesabagusr <mahesabagusraditya1@gmail.com>
2023-11-17 17:40:32 +0700

    task1

---
+++ index.js
@@ -0,0 +1,17 @@
+//
+function ganjilGenap(x) {
+
+}
+
+function piramida(X) {
+
+}
+
+function fizzBuzz(x) {
+
+}
+
+//TOLONG JANGAN DI HAPUS (NILAI PARAMETER BOLEH DI GANTI SESUAI KEBUTUHAN)
+ganjilGenap(10)
+piramida(10);
+fizzBuzz(100)
